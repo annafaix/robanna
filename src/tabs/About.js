623ff75e4a2e54814601c7e7d9333fab2707af28
golgor <golgafrincham@gmail.com>
2018-11-07 20:50:58 +0100

    style changes and added text to about

--- src/tabs/About.js
+++ src/tabs/About.js
@@ -12,7 +12,71 @@ export default class About extends Component {
       <div className="tab">
         <h1>Hur allt började</h1>
         <p>
-          Anna plugade svenska, teater i Sverige, couchsurfing, resa till Brno, Kroatien tillsammans, Budapest,  första träffen med föräldrar, Rysy, jul i Kezmarok, eurotrip, flytt till Sverige, Robert har börjat lära sig slovakiska
+          Jo, det frågar vi oss också! Var det kanske när Robert bestämde sig
+          att hitta en "Anna" redan när har var liten eftersom han ville att
+          Mikaela skulle heta så? Eller började det när Anna bestämde sig att
+          plugga svenska utan att ens besöka Sverige innan?
+        </p>
+        <p>
+          Vårt första möte var tack vare en av Annas kompisar, Jana. Den
+          4:e februari 2013 behövde Anna och hennes kompis Kristina stanna i
+          Göteborg för en natt, deras flyg från Göteborg gick jättetidigt dagen
+          efter. Robert var en erfaren couchsurfare som tidigare hostat Jana
+          och Anna fick en lapp med Roberts adress och telefonnummer på. Jana sa
+          skämtsamt "jag har hittat en brudgum till dig Anna!", Anna var inte
+          övertygad vid det tillfället men gick ändå med på att stanna för
+          den natten... Detta första möte var dock början på något som ingen vi
+          inte kunde föreställa oss då!
+        </p>
+        <p>
+          Några månader senare besökte Robert och några kompisar Brno där
+          Anna bodde eftersom han hostat många personer från just Brno. Vem
+          vill inte spendera en långhelg i Tjeckien? Robert och en kompis Kim
+          bodde hos Anna och hennes kompisar i en lägenhet och det blev en
+          långhelg med många roliga händelser. Vi umgicks allt mer och besöket
+          avslutades med en av de första pussarna.
+        </p>
+        <p>
+          Lite senare på våren hade Anna möjligheten att besöka Sverige
+          tillsammans med en teatergrupp som skulle till Lidköping. Robert
+          tog tillfället i akt att med hjälp av Annas kompisar iscensätta
+          en överraskning och tog tåget till Lidköping för att stanna med
+          teatergruppen i deras boende. Direkt efter så åkte Anna med Robert
+          hem till Göteborg och stannade i några dagar extra. Det var då vi
+          bestämde oss för att försöka bli ett par, trots det besvärliga
+          avståndet.
+        </p>
+        <p>
+          Det "enklaste" sättet för oss att träffa lite då och då var i den
+          mysiga Budapest. Robert tog billigt flyg från Göteborg och Anna tog
+          tåget från Brno. En rolig detalj var att det tog ungefär lika lång
+          tid att komma dit för oss båda. Att träffas i Budapest var en
+          tradition som vi höll fasta på under hela tiden Anna bodde i
+          Tjeckien.
+        </p>
+        <p>
+          Samma sommar, några månader efter överraskningen i Lidköping så
+          åkte vi på semester tillsammans. Flög ner till Kroatien, möttes där?
+        </p>
+        <p>
+          Lite senare, Anna kom till Sverige? Besökte Åsa? Google photo säger det.
+          därefter kom Robert till Slovakien och
+          träffade Annas familj... (och blev direkt omtyckt av hennes pappa :) )
+        </p>
+        <p>
+          Då Martin var med så åkte vi också hem till honom i
+          Kroatien. Vi åkte sen tillbaka hemåt via Italien, Frankrike,
+          Nederländerna.
+        </p>
+        <p>
+          Sommaren 2014 åkte vi på en galen Eurotrip med totalt sju galna
+          människor. Totalt var vi borta i ungefär en månad och vi besökte
+          Annas föräldrar i Slovakien och bodde hemma hos dem i några dagar.
+          Resan avslutades i Göteborg för både Robert och Anna då vi flyttade
+          ihop. För oss var det början på ett liv tillsammans.
+        </p>
+        <p>
+          The rest is history, eller vad är det man brukar säga?
         </p>
       </div>
     )
--- src/tabs/Whenwhere.js
+++ src/tabs/Whenwhere.js
@@ -13,14 +13,20 @@ export default class Whenwhere extends Component {
     return(
       <div className="tab">
         <h1>Vår bröllopsdag</h1>
-        <h3>börjar med vigseln kl. 14.00 i slottskapellet i Kežmarok...</h3>
+        <h2>Vigsel</h2>
+        <p>
+        Klockan 14 börjar vigseln i slottskapellet i Kežmarok.<br/>
         <img src={church} className="kapell" alt="kapell"/>
         <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2610.218618007195!2d20.431034315413388!3d49.139473488828436!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x473e3dcd16839b75%3A0xfac8be68a1bba1ac!2zS2XFvm1hcnNrw70gaHJhZA!5e0!3m2!1scs!2sse!4v1541285684954"
         width="400" height="450"
         frameBorder="0"
         allowFullScreen
         title="maps-kapell"></iframe>
-        <h3>...och fortsätter med en stor fest till morgondagen lite längre bort! Yeeey :D </h3>
+        </p>
+        <h2>Fest!</h2>
+        <p>
+        Efter vigsel kommer det bli fest i en lokal som ligger på gångavstånd
+        till kapellet. Festen kommer fortsätta till morgondagen! Yeeey :D <br/>
         <img src={lokal} className="lokalen" alt="lokalen"/>
         <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2609.924683973154!2d20.430866253789475!3d49.145054391943425!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x473e3dcce4377367%3A0x4cf92f896d8dea29!2sVladim%C3%ADr+Vadel+VK+-+VADEL!5e0!3m2!1scs!2sse!4v1541286346533"
         width="80%"
@@ -28,6 +34,7 @@ export default class Whenwhere extends Component {
         frameBorder="0"
         allowFullScreen
         title="maps-party"></iframe>
+        </p>
       </div>
     )
   }
