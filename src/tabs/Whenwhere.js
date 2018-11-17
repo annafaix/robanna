d26ba003c073c7989c9b118c861ca89b629bb7ca
annafaix <annafaix@gmail.com>
2018-11-17 23:01:18 +0100

    change in when-where

--- src/tabs/Whenwhere.js
+++ src/tabs/Whenwhere.js
@@ -16,25 +16,24 @@ export default class Whenwhere extends Component {
         <h2>Vigsel</h2>
         <p>
         Klockan 14 börjar vigseln i slottskapellet i Kežmarok.<br/>
+        </p>
         <img src={church} className="kapell" alt="kapell"/>
         <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2610.218618007195!2d20.431034315413388!3d49.139473488828436!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x473e3dcd16839b75%3A0xfac8be68a1bba1ac!2zS2XFvm1hcnNrw70gaHJhZA!5e0!3m2!1scs!2sse!4v1541285684954"
-        width="400" height="450"
-        frameBorder="0"
+        width="90%" height="350"
+        frameBorder="1px"
         allowFullScreen
         title="maps-kapell"></iframe>
-        </p>
         <h2>Fest!</h2>
         <p>
-        Efter vigsel kommer det bli fest i en lokal som ligger på gångavstånd
-        till kapellet. Festen kommer fortsätta till morgondagen! Yeeey :D <br/>
+        Efter vigsel kommer det bli fest i en lokal som ligger väldigt nära från kapellet. Festen kommer fortsätta till morgondagen! Yeeey :D </p>
         <img src={lokal} className="lokalen" alt="lokalen"/>
         <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2609.924683973154!2d20.430866253789475!3d49.145054391943425!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x473e3dcce4377367%3A0x4cf92f896d8dea29!2sVladim%C3%ADr+Vadel+VK+-+VADEL!5e0!3m2!1scs!2sse!4v1541286346533"
-        width="80%"
-        height="250"
-        frameBorder="0"
+        width="90%"
+        height="350"
+        frameBorder="1"
         allowFullScreen
         title="maps-party"></iframe>
-        </p>
+       
       </div>
     )
   }
